e65e3ed2a2b0e7ff43652fb9a3f2738a701b0745
hachiri <43362632+hachiri@users.noreply.github.com>
2019-02-19 18:30:12 +0800

    DFS

---
+++ HDU/2553.cpp
@@ -0,0 +1,49 @@
+//15ms_1380kB
+#include <bits/stdc++.h>
+using namespace std;
+
+int i, n, ans, pl[100], w[100];
+bool c[30], a[30], b[30];
+void dfs(int p)
+{
+	if (p > n)
+		ans++;
+	else
+		for (i = 1; i <= n; i++)
+		{
+			if (c[i] == false && a[p + i - 1] == false && b[n - p + i] == false)
+			{
+				pl[p] = i;
+				c[i] = true;
+				a[p + i - 1] = true;
+				b[n - p + i] = true;
+				dfs(p + 1);
+				i = pl[p];
+				c[i] = false;
+				a[p + i - 1] = false;
+				b[n - p + i] = false;
+			}
+		}
+}
+
+int main()
+{
+	memset(w, -1, sizeof(w));
+	while (scanf("%d", &n))
+	{
+		if (n == 0)	break;
+		if (w[n] != -1)	printf("%d\n", w[n]);
+		else
+		{
+			memset(pl, 0, sizeof(pl));
+			memset(a, false, sizeof(a));
+			memset(b, false, sizeof(b));
+			memset(c, false, sizeof(c));
+			ans = 0;
+			dfs(1);
+			w[n] = ans;
+			printf("%d\n", ans);
+		}
+	}
+	return 0;
+}
